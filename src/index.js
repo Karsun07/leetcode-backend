745a0777606d68263dbc6842ca16855c2e10ee7b
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-06-22 17:08:54 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const app=express();
+require('dotenv').config();
+const main=require("./config/database");
+const cookieParser=require("cookie-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+main()
+.then(async ()=>{
+    console.log("Connected to DB");  
+    app.listen(process.env.PORT_NUMBER,()=>{
+    console.log("Listening at Port "+process.env.PORT_NUMBER);
+})
+})
+.catch(err=>{
+    console.log("Error Occured : "+err);
+})
+
+
