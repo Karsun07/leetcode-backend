d02691d77203b1e9fa21d1bd944037e62032e3af
Karsun07 <159122311+Karsun07@users.noreply.github.com>
2026-07-14 15:40:07 +0530

    adbj

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 const mongoose=require("mongoose");
 
 async function main(){
+    if(!process.env.DB_CONNECT_STRING) throw new Error("DB connect string not present in environment variable.");
+    
     await mongoose.connect(process.env.DB_CONNECT_STRING);
 }
 module.exports=main;
